ee122977e5e2ba2d20f7db2d1bbee469d9bdb5a4
puze8681 <rsef0601@naver.com>
2017-02-28 06:12:30 +0900

    fuck;;

---
+++ Desktop/FetchApp/app/src/main/java/com/example/parktaejun/fetchapp/InfoApp.java
@@ -0,0 +1,8 @@
+package com.example.parktaejun.fetchapp;
+
+/**
+ * Created by parktaejun on 2017. 2. 28..
+ */
+
+public class InfoApp {
+}
--- Desktop/FetchApp/app/src/main/java/com/example/parktaejun/fetchapp/MainActivity.java
+++ Desktop/FetchApp/app/src/main/java/com/example/parktaejun/fetchapp/MainActivity.java
@@ -1,13 +1,25 @@
 package com.example.parktaejun.fetchapp;
 
+import android.content.pm.PackageManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
     ListView list;
+    private static final String TAG = MainActivity.class.getSimpleName();
+    // 메뉴 KEY
+    private final int MENU_DOWNLOAD = 0;
+    private final int MENU_ALL = 1;
+    private int MENU_MODE = MENU_DOWNLOAD;
 
+    private PackageManager pm;
+
+    private View mLoadingContainer;
+    private ListView mListView = null;
+    private com.example.parktaejun.fetchapp.MainActivity.IAAdapter mAdapter = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
