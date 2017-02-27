4ee2a7ba5469ba03c3cd752eb3ee881c53c87767
puze8681 <rsef0601@naver.com>
2017-02-28 05:47:59 +0900

    add item

---
+++ Desktop/InstalledApp/app/src/main/java/com/example/parktaejun/installedapp/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.parktaejun.installedapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView list;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        list = (ListView)findViewById(R.id.list_view);
+    }
+}
